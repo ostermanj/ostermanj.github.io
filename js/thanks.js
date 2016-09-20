9a85238f161f9f29215747b1c5e38e6375c0fd65
John Osterman <ostermanj@gmail.com>
2016-09-20 16:50:34 -0400

    Adds hidden attribute to thanks message for accessibility purposes

--- js/thanks.js
+++ js/thanks.js
@@ -1,6 +1,8 @@
 var url = window.location.href;
 if (url.indexOf('?thanks') !== -1){
     document.getElementById('thanks').style.display  = 'block';
+    document.getElementById('thanks').removeAttribute('hidden');
+    
     setTimeout(function(){
            document.getElementById('thanks').className = 'processed';
     }, 200);
