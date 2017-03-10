caff85f9d9e704de26a19e72e6f69b7f798575bc
John Osterman <ostermanj@gmail.com>
2017-03-10 16:08:13 -0500

    Hover bars are in

--- scorecard/js/scripts.js
+++ scorecard/js/scripts.js
@@ -5,82 +5,225 @@
  */
 
 var Chart = function(el) {
-        
-        var chart = this;
-        this.el = el;
-        this.chartMinMaxDomain();
-    };
+
+    var chart = this;
+    this.el = el;
+    this.chartMinMaxDomain();
+};
 
 Chart.prototype = {
-       chartMinMaxDomain: function(){
-            var chart = this;
-            var numericValues = [];
-            this.domain = [];
-            model.data.forEach(function(c){
-                c.values.forEach(function(d){
-                    numericValues.push(d.value);
-                    if (chart.domain.indexOf(d.domain) === -1){
-                        chart.domain.push(d.domain);
-                    }
-                });
+    extendPrototype: function(destinationPrototype, obj) {
+        for (var i in obj) {
+            destinationPrototype[i] = obj[i];
+        }
+    },
+    chartMinMaxDomain: function() {
+        var chart = this;
+        var numericValues = [];
+        this.domain = [];
+        model.data.forEach(function(c) {
+            c.values.forEach(function(d) {
+                numericValues.push(d.value);
+                if (chart.domain.indexOf(d.domain) === -1) {
+                    chart.domain.push(d.domain);
+                }
             });
-            
-            chart.domain.sort();
-            this.max = d3.max(numericValues);
-            this.min = d3.min(numericValues);
-            
-            this.protoSetup();
-       },
-       protoSetup: function(){
-            var chart = this; // should be able to use app.chart ??
-            this.margin = {
-                    top: 12,
-                    right: 0,
-                    bottom: 0,
-                    left: 0
-                };
-                
-            this.strokeWidth = 3;
+        });
 
-            this.svgWidth = 106;
-            
-            this.svgHeight = this.svgWidth;
-            this.previousValue = null;
+        chart.domain.sort();
+        this.max = d3.max(numericValues);
+        this.min = d3.min(numericValues);
 
-            chart.y = d3.scaleLinear()
-                .domain([chart.min, chart.max])
-                .range([2, ( chart.svgHeight / 2 ) - chart.strokeWidth - chart.margin.top]);
+        this.protoSetup();
+    },
+    protoSetup: function() {
+        var chart = this; 
+        
+
+        this.svgs = d3.select(this.el)
+            .selectAll('svg')
+            .data(model.data).enter()
+            .append('svg')
+            .attr('id', function(d) {
+                return d.key;
+            })
+            .attr('width', this.svgWidth)
+            .attr('height', this.svgHeight)
+            .on('mouseover', this.hoverIn)
+            .on('mouseout', this.hoverOut);
 
 
-            this.svgs = d3.select(this.el)
-                .selectAll('svg')
-                .data(model.data).enter()
-                .append('svg')
-                .attr('id', function(d){
-                    return d.key;
-                })
-                .attr('width', this.svgWidth)
-                .attr('height', this.svgHeight)
-                .on('mouseover', this.hoverIn) 
-                .on('mouseout', this.hoverOut);
-                
+        this.svgs.append('text')
+            .text(function(d) {
+                return d.key;
+            })
+            .attr('alignment-baseline', 'after-edge')
+            .attr('class', 'country-label')
+            .attr('y', this.svgHeight)
+            .attr('x', this.svgWidth / 2)
+            .attr('text-anchor', 'middle');
 
-            this.svgs.append('text')
-                .text(function(d){
-                    return d.key;
-                })
-                .attr('alignment-baseline', 'after-edge')
-                .attr('class','country-label')
-                .attr('y', this.svgHeight)
-                .attr('x',this.svgWidth / 2)
-                .attr('text-anchor', 'middle');
+        this.setup();
 
     },
-       hoverIn: function(){
+    hoverIn: function() {
         d3.select(this)
-        .select('g.circles')
-        .transition().duration(200)
-        .attr('opacity', 0);
+            .select('g.circles')
+            .transition().duration(200)
+            .attr('opacity', 0);
+
+
+
+
+    },
+    hoverOut: function() {
+        d3.select(this)
+            .select('g.circles')
+            .transition().duration(200)
+            .attr('opacity', 1);
+    }
+
+
+};
+
+/*
+ * CIRCLE CHART CONSTRUCTOR
+ */
+
+var CircleChart = function(el) {
+     
+    var chart = this;
+     this.margin = {
+                top: 12,
+                right: 12,
+                bottom: 12,
+                left: 12
+            };
+
+    this.strokeWidth = 3;
+
+    this.svgWidth = 106;
+
+    this.svgHeight = this.svgWidth;
+    this.previousValue = null;
+    
+    this.extendPrototype(CircleChart.prototype, circleChartExtension);
+    Chart.call(this, el);
+
+}
+
+CircleChart.prototype = Object.create(Chart.prototype);
+
+var circleChartExtension = {
+
+
+    setup: function() {
+
+        var chart = this;
+
+
+        this.y = d3.scaleLinear()
+                .domain([chart.min, chart.max])
+                .range([2, (chart.svgHeight / 2) - chart.strokeWidth - chart.margin.top]);
+
+        this.gs = this.svgs.append('g')
+            .attr('class', 'circles')
+            .attr('opacity', 1);
+
+        this.gs.selectAll('circle')
+            .data(function(d) {
+                var sorted = d.values.sort(function(a, b) {
+                    return b.value - a.value;
+                })
+                return sorted;
+            }) // numerical values of each
+            .enter().append('circle')
+            .attr('cx', chart.svgWidth / 2)
+            .attr('cy', chart.svgHeight / 2)
+            .attr('r', 0)
+            .attr('stroke-width', this.strokeWidth)
+            .attr('class', function(d) {
+                return d.domain;
+            });
+
+        this.adjustRadii();
+    },
+    adjustRadii: function() {
+
+        var chart = this;
+
+
+        this.svgs.selectAll('circle')
+            .transition().delay(function(d, i) {
+                return 200 + (i * 20);
+            }).duration(1000).ease(d3.easeBounce)
+            .attr('r', function(d, i, array) {
+                
+                var radius = chart.y(d.value);
+
+                if (chart.previousValue) {
+
+
+                    if (chart.previousValue - radius < chart.strokeWidth && chart.previousValue >= radius) {
+
+
+
+                        radius = chart.previousValue - chart.strokeWidth;
+                    } else if (radius - chart.previousValue < chart.strokeWidth && radius >= chart.previousValue) {
+                        radius = chart.previousValue + chart.strokeWidth;
+                    }
+                }
+                if (i === array.lenth - 1) {
+                    chart.previousValue = null;
+                } else {
+                    chart.previousValue = radius;
+                }
+                return radius;
+            });
+        this.hoverCharts();
+    },
+
+    hoverCharts: function(){
+        var chart = this;
+        
+        this.barY = d3.scaleLinear()
+            .domain([chart.min, chart.max])
+            .range([0, chart.svgHeight - chart.margin.top]);
+
+        this.barX = d3.scaleBand()
+            .domain(chart.domain)
+            .range([chart.margin.left, chart.svgWidth - chart.margin.right]);
+
+        this.gs = this.svgs.append('g')
+            .attr('class', 'columns')
+            .attr('opacity', 1)
+            .selectAll('rect')
+             .data(function(d) {
+                    var sorted = d.values.sort(function(a,b){
+                        return b.value - a.value;
+                    })
+                    return sorted;
+            })
+          .enter().append('rect')
+          .attr('width', chart.barX.bandwidth())
+          .attr('x', function(d){
+            return chart.barX(d.domain);
+          })
+          .attr('height', function(d){
+            return chart.barY(d.value);
+          })
+          .attr('y', function(d){
+            return chart.svgHeight - chart.margin.bottom - chart.barY(d.value) - chart.strokeWidth;
+          });
+    }
+
+};
+
+/*
+ * HOVER CHART
+ */
+
+
 
         /*d3.select(this)
           .append('g')
@@ -100,64 +243,6 @@ Chart.prototype = {
                 .padding(0.33);
 
           })*/
-       },
-       hoverOut: function(){
-         d3.select(this)
-            .select('g.circles')
-            .transition().duration(200)
-            .attr('opacity', 1);
-       },
-       adjustLength: function(){
-            
-            var chart = this;
-            
-            
-            this.svgs.selectAll('circle')
-            .transition().delay(function(d,i){
-                return 200 + (i * 20);
-            }).duration(1000).ease(d3.easeBounce)
-            .attr('r', function(d,i,array) {                
-                    var radius = chart.y(d.value);
-                    
-                    if (chart.previousValue){
-                        
-                        
-                        if (chart.previousValue - radius < chart.strokeWidth && chart.previousValue >= radius) {
-                            
-                            
-                            
-                            radius = chart.previousValue - chart.strokeWidth;
-                        } else if (radius - chart.previousValue < chart.strokeWidth && radius >= chart.previousValue){
-                            radius = chart.previousValue + chart.strokeWidth;
-                        }
-                    }
-                    if ( i === array.lenth - 1 ){
-                        chart.previousValue = null;
-                    } else {
-                        chart.previousValue = radius;
-                    }
-                    return radius;
-                }); 
-        }
-
-};
-
-/*
- * CIRCLE CHART CONSTRUCTOR
- */
-
-    var CircleChart = function(el){
-
-        this.extendPrototype(CircleChart.prototype, circleChartExtension); 
-        Chart.call(this, el);
-
-    }
-
-    CircleChart.prototype = Object.create(Chart.prototype); 
-
-    var circleChartExtension = {
-        
-    };
 
 
 /*
@@ -168,7 +253,7 @@ Chart.prototype = {
 var model = {
     data: [],
     initialize: function(json) {
-        
+
         json.forEach(function(obj) { //iterate over each object of the data array
             for (var key in obj) { // iterate over each property of the object
                 if (obj.hasOwnProperty(key)) {
@@ -182,18 +267,18 @@ var model = {
             })
             .entries(json);
 
-        
+
         model.data = nested;
 
         controller.makeCharts();
 
-        }
+    }
 
-    };
+};
 
 var controller = {
-    makeCharts: function(){
-         new Chart('#chart-0');
+    makeCharts: function() {
+        new CircleChart('#chart-0');
     }
 };
 
