95626f1c7b7db31698c7e1952d3b1f82a734f790
John Osterman <ostermanj@gmail.com>
2022-07-29 12:52:03 -0400

    Moves md to html server side

--- src/routes/post/[slug].svelte
+++ src/routes/post/[slug].svelte
@@ -1,8 +1,6 @@
 <script context="module">
     import Tags from '$components/Tags.svelte';
     import Authors from '$components/Authors.svelte';
-    import { getAsset } from '$utils/contentful';
-    import { markdown } from 'markdown';
 </script>
 <script lang="ts">
     export let title: string;
@@ -10,41 +8,6 @@
     export let tags:[];
     export let datePublished: string;
     export let authors:[];
-    let htmlTree = markdown.toHTMLTree(body);
-    sourceSetify(htmlTree);
-    console.log(htmlTree);
-    let bodyHTML = markdown.renderJsonML(htmlTree);
-    // TO DO: SHOULD PRPBABLY DO THIS IN THE PAGE ENDPOINT INSTEAD. RETURN HTML, NOT MARKDOWN
-    /**
-     * 
-     * @param htmlTree string
-     * 
-     * the body of the blogpost is in markdown. it may contain images that would render
-     * as stright up <img> tags. better to render them instead as <picture>s with <sources>
-     * to make use of next generation image formats, avif, webp, that the contentful CMS
-     * makes easily available
-     */
-    function sourceSetify(htmlTree: []): void {
-        async function findImageTag(branch){
-            var hasBeenModified = false;
-            if ( branch[0] == 'img' ){
-                branch[0] = 'picture';
-                let attributes = Object.assign({}, branch[1]);
-                let imgID = attributes.src.split('/')[4];
-                let imageData = await getAsset(imgID);
-                console.log(imageData);
-                branch.splice(1)
-                branch.push(...['avif', 'webp'].map(format => {
-                    return ['source', {srcset: attributes.src + `?fm=${format}`, type: `image/${format}`}]
-                }));
-                branch.push(['img', attributes]);
-                hasBeenModified = true;
-            } else if ( Array.isArray(branch[1]) && !hasBeenModified){
-                findImageTag(branch[1])
-            }
-        }
-        htmlTree.forEach(findImageTag)
-    }
 </script>
 <article>
     <header>
@@ -53,5 +16,5 @@
         <Authors {authors} />
         <p>Published <time datetime={datePublished}>{new Date(datePublished).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</time></p>
     </header>
-    {@html bodyHTML}
+    {@html body}
 </article>
--- utils/contentful.ts
+++ utils/contentful.ts
@@ -1,5 +1,6 @@
 import type { JSONValue } from '@sveltejs/kit/types/private';
 import contentful, { type Entry, type EntryCollection } from 'contentful';
+import { markdown } from 'markdown';
 const client = contentful.createClient({
     space: tS(process.env.C_SPACE),
     //environment: 'master',
@@ -9,9 +10,50 @@ function tS(value: string | undefined){
     if ( typeof value == 'string') return value;
     return '';
 }
-export function getBlogById(id:string){
+async function toHTML(md:string){
+    let htmlTree = markdown.toHTMLTree(md);
+    htmlTree = await sourceSetify(htmlTree);
+    return markdown.renderJsonML(htmlTree);
+}
+function sourceSetify(htmlTree: []): Promise<[]> {
+    return new Promise(function(resolve){
+        const branchPromises = [];
+        async function findImageTag(branch){
+            if ( branch[0] == 'img' ){
+                branch[0] = 'picture';
+                const attributes = Object.assign({}, branch[1]);
+                const imgID = attributes.src.split('/')[4];
+                const imageData = await getAsset(imgID);
+                attributes.width = `${imageData.fields.file.details.image?.width}px`;
+                attributes.height = `${imageData.fields.file.details.image?.height}px`;
+                branch.splice(1)
+                branch.push(...['avif', 'webp'].map(format => {
+                    return ['source', {srcset: attributes.src + `?fm=${format}`, type: `image/${format}`}]
+                }));
+                branch.push(['img', attributes]);
+                return branch;
+            } else if ( Array.isArray(branch[1]) ){
+                return findImageTag(branch[1]);
+            } else {
+                return branch;
+            }
+        }
+        htmlTree = htmlTree.splice(1);
+        htmlTree.forEach(branch => {
+            branchPromises.push(findImageTag(branch));
+        });
+        Promise.all(branchPromises).then(() => {
+            resolve(htmlTree);
+        });
+    });
+
+}
+export async function getBlogById(id:string){
     // below asserts that return is not Promise<Entry<unknow>> but ... 
-    return client.getEntry(id) as Promise<Entry<JSONValue>>;
+    const response = await client.getEntry(id) as Entry<JSONValue>;
+    response.fields.body = await toHTML(response.fields.body);
+    console.log(response.fields.body);
+    return response;
 }
 export function getPageContent(page: string){
     console.log(page);
