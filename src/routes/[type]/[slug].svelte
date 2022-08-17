a6c3bbce4a138faa14e8108234246f98e38932b8
John Osterman <ostermanj@gmail.com>
2022-08-17 07:55:26 -0400

    WIP

--- src/routes/[type]/[slug].svelte
+++ src/routes/[type]/[slug].svelte
@@ -1,6 +1,7 @@
 <script context="module">
     import Tags from '$components/Tags.svelte';
     import Authors from '$components/Authors.svelte';
+    import Sprite from '$components/Sprite.svelte';
 </script>
 <script lang="ts">
     export let title: string;
@@ -13,9 +14,27 @@
     export let repository = '';
 </script>
 <style>
+    header {
+        margin-bottom: 4rem;
+    }
     .date {
-        margin-bottom: 3em;
         color: var(--text-color-light);
+        margin-bottom: 0;
+    }
+    .external-link {
+        
+    }
+    .external-link > div:first-child {
+       flex-shrink: 0;
+    }
+    .external-link > div:last-child {
+       width: calc(100% - 35px);
+       max-width: 300px;
+       white-space: nowrap;
+       overflow: hidden;
+       text-overflow: ellipsis;
+       position: relative;
+       top: 2px;
     }
 </style>
 <article class="article-post">
@@ -25,13 +44,27 @@
         {#if authors}
         <Authors {authors} />
         {/if}
-        <p class="date"><span class="vsh">Published</span> <time datetime={datePublished}>{new Date(datePublished).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</time></p>
+        <p class="date">
+            <span class="vsh">Published</span>
+            <time datetime={datePublished}>{new Date(datePublished).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</time>
+            | { contentType == 'project' ? 'Project' : 'Blog post'}
+        </p>
         {#if contentType == 'project'}
             {#if link}
-                <a href="{link}">{link}</a>
+                <div class="fx ai-c g0-5 external-link">
+                    <div class="fx ai-c">
+                        <Sprite id="www" />
+                    </div>
+                    <div>In situ: <a href="{link}">{link.replace(/https?:\/\//, '')}</a></div>
+                </div>
             {/if}
             {#if repository}
-                <a href="{repository}">{repository}</a>
+                <div class="fx ai-c g0-5 external-link">
+                    <div class="fx ai-c">
+                        <Sprite id="github" />
+                    </div>
+                    <div>Code: <a href="{repository}">{repository.replace(/https?:\/\//, '')}</a></div>
+                </div>
             {/if}
         {/if}
     </header>
