6206733033e87503485287cbd9faf423c3ad97cd
John Osterman <ostermanj@gmail.com>
2022-08-26 11:58:28 -0400

    Remove base from redirect

--- src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
+++ src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
@@ -1,9 +1,8 @@
 <script>
-    import { base } from '$app/paths';
     export let data;
     const { redirect } = data;
 </script>
 <svelte:head>
-    <meta http-equiv="refresh" content="0;url={base}{redirect}" />
+    <meta http-equiv="refresh" content="0;url={redirect}" />
 </svelte:head>
 <p>Redirecting to new location . . .</p>
