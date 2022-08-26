eff7ab46d8f0bd9c3fb01a880abec64724fbc4ef
John Osterman <ostermanj@gmail.com>
2022-08-26 12:08:04 -0400

    No router or hydrate on redirect page

--- src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
+++ src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     export let data;
     const { redirect } = data;
+    export const router = false;
+    export const hydrate = false;
 </script>
 <svelte:head>
     <meta http-equiv="refresh" content="0;url={redirect}" />
