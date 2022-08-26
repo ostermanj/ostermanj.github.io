463668bf5ec65dbf2f1fc78e19149dfee503053e
John Osterman <ostermanj@gmail.com>
2022-08-26 12:35:29 -0400

    Revert "WIP adds trailing slash to redirect"
    This reverts commit e88946f7d570bc4779eff2701d6b137810f4f9e4.

--- src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
+++ src/routes/[type]/[year]/[month]/[day]/[slug]/+page.svelte
@@ -5,6 +5,6 @@
     export const hydrate = false;
 </script>
 <svelte:head>
-    <meta http-equiv="refresh" content="0;url={redirect}/" />
+    <meta http-equiv="refresh" content="0;url={redirect}" />
 </svelte:head>
 <p>Redirecting to new location . . .</p>
