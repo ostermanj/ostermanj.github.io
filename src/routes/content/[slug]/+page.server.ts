99e211d3121cf2bc43ace64f660c7b95418091d5
John Osterman <ostermanj@gmail.com>
2022-08-26 13:28:47 -0400

    Inserts base path into domain rel. urls from cms

--- src/routes/content/[slug]/+page.server.ts
+++ src/routes/content/[slug]/+page.server.ts
@@ -4,15 +4,17 @@ import type { PageServerLoad } from "../$types";
 import { getBlogById } from '$utils/contentful';
 import _slugsToIDs from '$utils/bloglist.json' assert {type: 'json'};
 import { error } from '@sveltejs/kit';
+import { base } from '$app/paths';
 const slugsToIDs: Record<string, string> = _slugsToIDs;
 export const load:PageServerLoad<ResponseBody> = async function _GET( {params} ) {
   const id:string = slugsToIDs[params.slug];
-  if ( id === undefined ){
+  if ( id === undefined ){  
     throw error(404, {statusText: "Not Found"});
   }
   try {
       const response = await getBlogById(id);
       response.fields.contentType = response.sys.contentType.sys.id;
+      response.fields.body = response.fields.body.replace(/href="\/(?!\/)/g, `href="${base}/`);
       return response.fields
     }  catch(e){
         const errorMessage = JSON.parse(e.message);
