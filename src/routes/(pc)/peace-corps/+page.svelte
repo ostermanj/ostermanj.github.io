69fa0669b60ad547d7a1bcdec0c652eafe106fc5
John Osterman <ostermanj@gmail.com>
2022-10-24 09:49:31 -0400

    As external script

--- src/routes/(pc)/peace-corps/+page.svelte
+++ src/routes/(pc)/peace-corps/+page.svelte
@@ -17,56 +17,10 @@
     let currentPage = 1;
     let pageSize = 10;
     $: paginatedItems = paginate({ items, pageSize, currentPage });
-    function callback(entries, observer){
-        entries.forEach(entry => {
-            if (entry.isIntersecting){
-                requestAnimationFrame(() => {
-                    window._map.flyTo({
-                        center: [-11.0813, 21.1456],
-                        zoom: 3.9
-                    });
-                    observer.disconnect();
-                })
-            }
-        })
-    }
-    onMount(() => {
-        console.log('onMount!');
-        const container = document.getElementById('map-cont');
-        container?.insertAdjacentHTML('afterbegin','<div id="map-cont--inner"></div>');
-        const timeout = setInterval(() => {
-            console.log('nope');
-            if (window.mapboxgl){
-                console.log('yep');
-                initMap()
-            }
-        })
-        function initMap(){
-            clearInterval(timeout);
-            mapboxgl.accessToken = 'pk.eyJ1Ijoib3N0ZXJtYW5qIiwiYSI6ImNsOWl5NmF5ZTA4ODgzd28wczZ3bm9oYm0ifQ.qLNG2qiKlw8RkjFlHwsHhQ';
-                window._map = new mapboxgl.Map({
-                container: 'map-cont--inner', // container ID
-                style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
-                center: [-100.4544, 37.0351], // starting position [lng, lat]
-                // center: [-11.0813, 21.1456], // starting position [lng, lat]
-                zoom: 1.256, // starting zoom
-                // zoom: 3.9, // starting zoom
-                projection: 'globe' // display the map as a 3D globe
-                });
-            window._map.scrollZoom.disable();
-            window._map.on('load', () => {
-                const options = {
-                    root: null,
-                    threshold: 1
-                };
-                const observer = new IntersectionObserver(callback, options);
-                observer.observe(container);
-            })
-        }
-    })
 </script>
 <svelte:head>
     <script defer src='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.js'></script>
+    <script defer src='/init-map.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css' rel='stylesheet' />
 </svelte:head>
 <section>
---
+++ static/init-map.js
@@ -0,0 +1,46 @@
+(function(){
+    function callback(entries, observer){
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                requestAnimationFrame(() => {
+                    window._map.flyTo({
+                        center: [-11.0813, 21.1456],
+                        zoom: 3.9
+                    });
+                    observer.disconnect();
+                })
+            }
+        })
+    }
+    const container = document.getElementById('map-cont');
+    container?.insertAdjacentHTML('afterbegin','<div id="map-cont--inner"></div>');
+    const timeout = setInterval(() => {
+        console.log('nope');
+        if (window.mapboxgl){
+            console.log('yep');
+            initMap()
+        }
+    })
+    function initMap(){
+        clearInterval(timeout);
+        mapboxgl.accessToken = 'pk.eyJ1Ijoib3N0ZXJtYW5qIiwiYSI6ImNsOWl5NmF5ZTA4ODgzd28wczZ3bm9oYm0ifQ.qLNG2qiKlw8RkjFlHwsHhQ';
+            window._map = new mapboxgl.Map({
+            container: 'map-cont--inner', // container ID
+            style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+            center: [-100.4544, 37.0351], // starting position [lng, lat]
+            // center: [-11.0813, 21.1456], // starting position [lng, lat]
+            zoom: 1.256, // starting zoom
+            // zoom: 3.9, // starting zoom
+            projection: 'globe' // display the map as a 3D globe
+            });
+        window._map.scrollZoom.disable();
+        window._map.on('load', () => {
+            const options = {
+                root: null,
+                threshold: 1
+            };
+            const observer = new IntersectionObserver(callback, options);
+            observer.observe(container);
+        })
+    }
+})();
