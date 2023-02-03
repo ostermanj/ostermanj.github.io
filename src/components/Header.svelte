282c141b33c5644784e4dc90e33e3cdbbf341d92
John Osterman <ostermanj@gmail.com>
2023-02-02 20:16:32 -0500

    WIP

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,14 +6,30 @@
 <style>
     header {
         border-top: 5px solid var(--color-secondary-1);
-        border-bottom: 1px solid var(--color-primary-1);
-        background-color: var(--color-background-2);
-        min-height: 56px;
+        /* border-bottom: 1px solid var(--color-primary-1); */
+        background: linear-gradient(to right, var(--color-primary-1-dark), var(--color-primary-1-bright), var(--color-primary-1-dark) );
         position: sticky;
         top: -150px;
         margin-block-end: 2rem;
-        padding: 22px 0;
         z-index: 2;
+        --mask:
+        radial-gradient(31.30px at 50% calc(100% - 42.00px),#000 99%,#0000 101%) calc(50% - 28px) 0/56px 100%,
+        radial-gradient(31.30px at 50% calc(100% + 28.00px),#0000 99%,#000 101%) 50% calc(100% - 14px)/56px 100% repeat-x;
+        -webkit-mask: var(--mask);
+        mask: var(--mask);
+        padding-block-end: 2px;
+        backdrop-filter: blur(5px);
+        
+        
+    }
+    .first-inner {
+        padding: 22px 0;
+        min-height: 56px;
+        width: 100%;
+        -webkit-mask: var(--mask);
+        mask: var(--mask);
+        background-color: var(--color-background-2);
+        transform: scale(1.01)
     }
     h1 {
         margin: 0;
@@ -40,19 +56,21 @@
     
 </style>
 <header class="fx jc-c">
-    <div class="fx fd-c ai-c jc-sb pi1 inner">
-        <h1 class="not-h1">
-            <a class="bw-0" href="{base}/">
-                <Name />
-                <span class="vsh">John Osterman</span>
-            </a>
-        </h1>
-        <Social />
-        <nav class="fx">
-            <a class="bw-0 nav-link" href="{base}/about">About</a>
-            <a class="bw-0 nav-link" href="{base}/list/blogPost">Blog posts</a>
-            <a class="bw-0 nav-link" href="{base}/list/project">Projects</a>
-            <a class="bw-0 nav-link" href="{base}/peace-corps">Peace Corps</a>
-        </nav>
+    <div class="fx fd-c ai-c first-inner">
+        <div class="fx fd-c ai-c jc-sb pi1 inner">
+            <h1 class="not-h1">
+                <a class="bw-0" href="{base}/">
+                    <Name />
+                    <span class="vsh">John Osterman</span>
+                </a>
+            </h1>
+            <Social />
+            <nav class="fx">
+                <a class="bw-0 nav-link" href="{base}/about">About</a>
+                <a class="bw-0 nav-link" href="{base}/list/blogPost">Blog posts</a>
+                <a class="bw-0 nav-link" href="{base}/list/project">Projects</a>
+                <a class="bw-0 nav-link" href="{base}/peace-corps">Peace Corps</a>
+            </nav>
+        </div>
     </div>
 </header>
