3597c71646401fe3710bd0698c9177640323d980
John Osterman <ostermanj@gmail.com>
2023-02-23 09:15:09 -0500

    Removes social item

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -20,11 +20,11 @@
 			><span class="vsh">Github</span> <Sprite id="github" /></a
 		>
 	</li>
-	<li>
+	<!-- <li>
 		<a rel="me" class="bw-0" title="Mastadon" href="https://uiuxdev.social/@ostermoster">
 			<span class="vsh">Mastodon</span> <Sprite id="mastodon" offset={1} /></a>
 		
-	</li>
+	</li> -->
 	<li>
 		<a class="bw-0" title="LinkedIn" href="//www.linkedin.com/in/john-osterman-32714012/"
 			><span class="vsh">LinkedIn</span> <Sprite id="linkedin" offset={-1} /></a
